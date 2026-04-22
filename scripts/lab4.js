fafaa920d7e37e385a6e6eadb036e9967ca90ad9
paulm130 <158609527+paulm130@users.noreply.github.com>
2026-04-21 19:51:27 -0700

    tried to fix wrong type error for discountPrices

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -36,6 +36,9 @@ function discountPrices(prices, discount) {
         discounted.push(discountedPrice);
     }
 
+    if(Array.isArray(discounted) !== true) {
+        return false;
+    }
     return discounted;
 }
 
